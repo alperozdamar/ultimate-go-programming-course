d24e5853f29e341d1eef8f093fc0d1e31bae7b0b
aozdamar <68610798+aozdamar@users.noreply.github.com>
2022-07-16 22:54:07 -0400

    Logger sample is added. (Drop Pattern)

---
+++ 11_context/11_3_Failure_Detection/logger/logger.go
@@ -0,0 +1,45 @@
+package logger
+
+import (
+	"fmt"
+	"io"
+	"sync"
+)
+
+// Logger ....
+type Logger struct {
+	ch chan string
+	wg sync.WaitGroup
+}
+
+// New .....
+func New(w io.Writer, cap int) *Logger {
+	l := Logger{
+		ch: make(chan string, cap),
+	}
+
+	l.wg.Add(1)
+	go func() {
+		defer l.wg.Done()
+		for v := range l.ch {
+			fmt.Fprint(w, v)
+		}
+	}()
+
+	return &l
+}
+
+// Close ....
+func (l *Logger) Close() {
+	close(l.ch)
+	l.wg.Wait()
+}
+
+// Println ....
+func (l *Logger) Println(v string) {
+	select {
+	case l.ch <- v:
+	default:
+		fmt.Println("DROP")
+	}
+}
